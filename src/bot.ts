a90ece17dea81b7650ed2723a4e8136295654ba3
Moji Raznahan <moji.rzn@gmail.com>
2024-08-01 09:57:47 -0700

    Fix type error in bot.s

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,14 @@
 import TelegramBot from 'node-telegram-bot-api';
 import config from './config';
 
+if (!config.telegramBotToken) {
+  throw new Error('Telegram bot token is not defined.');
+}
+
 const bot = new TelegramBot(config.telegramBotToken, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Welcome to IELTS Writing Bot! Use /submit to submit your essay.');
-});
-
-bot.onText(/\/submit/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Please send your essay as a text, PDF, or Word document.');
 });
 
 export default bot;
